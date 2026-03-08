39b822fa808821b74b3681da694f4b6c1d7f0348
Giovanni <giovanniayiahmensah@gmail.com>
2026-03-08 08:33:00 +0000

    Day00 - Hello World & Variables (The Comeback🔥🔥)

---
+++ python-to-ml-journey/phase-1-python-fundamentals/week-01-basics/day00_hello_world.py
@@ -0,0 +1,12 @@
+print("Welcome Back Sucker!!!")
+print("Here to try again ??")
+print("This is the last trial Man! we are killing it this time let's grind🔥🔥")
+
+#Variables 
+name = "Giovanni Ayiah-Mensah"
+age = "22"
+course = " BBA  Business Information Systems"
+print("")
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"I am reading {course} at the University of Education, Winneba")
